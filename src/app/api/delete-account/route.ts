fff5ff8425799ffc8529f062fb059d9bfb5dc0d0
Samyak-Waghmare <samyakwaghmare347@gmail.com>
2026-06-17 17:52:05 +0530

    cleaned up the code and made some minor adjustments to delete-account

---
+++ src/app/api/delete-account/route.ts
@@ -0,0 +1,53 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/options";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { User } from "next-auth";
+
+export async function DELETE() {
+  await dbConnect();
+
+  const session = await getServerSession(authOptions);
+  const user: User = session?.user as User;
+
+  if (!session || !session.user) {
+    return Response.json(
+      {
+        success: false,
+        message: "Not Authenticated",
+      },
+      { status: 401 }
+    );
+  }
+
+  try {
+    const deletedUser = await UserModel.findByIdAndDelete(user._id);
+
+    if (!deletedUser) {
+      return Response.json(
+        {
+          success: false,
+          message: "User not found",
+        },
+        { status: 404 }
+      );
+    }
+
+    return Response.json(
+      {
+        success: true,
+        message: "Account and all associated messages permanently deleted",
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error deleting user account", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Error deleting account",
+      },
+      { status: 500 }
+    );
+  }
+}
